30039da30a6fb43935d83162d6ed2580a9b600c2
jjones <jimmy.jones@tutanota.com>
2022-10-21 17:13:48 -0600

    stub out header and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
+	import Footer from '$lib/components/footer.svelte';
+	import Navbar from '$lib/components/navbar.svelte';
 	import '../app.postcss';
 </script>
 
+<Navbar />
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
+<Footer />
